7e67928a84ca0d1da0d150c81c8bf8ce8af11800
Seolcita <seolcita@gmail.com>
2021-05-03 15:12:39 -0600

    Sub Category CRUD

--- front/src/App.js
+++ front/src/App.js
@@ -21,7 +21,8 @@ import Wishlist from "./pages/user/Wishlist";
 import AdminDashboard from "./pages/admin/AdminDashboard";
 import CategoryCreate from "./pages/admin/category/CategoryCreate";
 import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
-
+import SubCreate from "./pages/admin/sub/SubCreate";
+import SubUpdate from "./pages/admin/sub/SubUpdate";
 
 import UserRoute from "./components/routes/UserRoute";
 import AdminRoute from "./components/routes/AdminRoute";
@@ -78,6 +79,9 @@ const App = () => {
           path="/admin/category/:slug"
           component={CategoryUpdate}
         />
+      <AdminRoute exact path="/admin/sub" component={SubCreate} />
+      <AdminRoute exact path="/admin/sub/:slug" component={SubUpdate} />
+
     </Switch>
     </>
   )
